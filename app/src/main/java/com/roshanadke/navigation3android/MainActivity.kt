a7ae77be554cf51018eb2500433d92642fbc5379
roshan129 <roshanadke2015@gmail.com>
2025-08-30 13:58:17 +0530

    necessary libraries added.

--- app/src/main/java/com/roshanadke/navigation3android/MainActivity.kt
+++ app/src/main/java/com/roshanadke/navigation3android/MainActivity.kt
@@ -20,28 +20,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             Navigation3AndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Navigation3AndroidTheme {
-        Greeting("Android")
     }
 }
